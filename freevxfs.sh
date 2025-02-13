81a372ccd36d8932d170b723a47e7cfb4a4590c9
bayron-cyber <emmanueladounvo408@gmail.com>
2025-02-13 15:36:35 +0100

    Update freevxfs.sh

--- freevxfs.sh
+++ freevxfs.sh
@@ -1,4 +1,3 @@
- Rôle : Désactiver freevxfs, un système de fichiers utilisé par HP-UX.
 #!/usr/bin/env bash
 echo "install freevxfs /bin/false" > /etc/modprobe.d/freevxfs.conf
 echo "blacklist freevxfs" >> /etc/modprobe.d/freevxfs.conf
