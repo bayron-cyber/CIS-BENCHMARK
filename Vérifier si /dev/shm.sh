131854c703ecc2eef207f52ef778f8976641fcc1
bayron-cyber <emmanueladounvo408@gmail.com>
2025-02-13 15:48:10 +0100

    Rôle : Sécuriser /dev/shm pour éviter des attaques mémoire partagée.

--- Vérifier si /dev/shm.sh
+++ Vérifier si /dev/shm.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 echo "tmpfs /dev/shm tmpfs defaults,nodev,nosuid,noexec 0 0" >> /etc/fstab
 mount -o remount,nodev,nosuid,noexec /dev/shm
 echo "/dev/shm partition secured."
