75af1a138b33bab291d2103b8d55d39f845472b6
bayron-cyber <emmanueladounvo408@gmail.com>
2025-02-13 15:47:11 +0100

    Rôle : Empêcher l'utilisation de périphériques de stockage USB.

--- usb-storage.sh
+++ usb-storage.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 echo "install usb-storage /bin/false" > /etc/modprobe.d/usb-storage.conf
 echo "blacklist usb-storage" >> /etc/modprobe.d/usb-storage.conf
 modprobe -r usb-storage
