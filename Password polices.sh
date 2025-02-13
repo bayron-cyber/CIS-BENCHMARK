009c75f764d56c9905656c2c81c05bd3e0f40d10
Kpodedji <162030419+Kpodedji@users.noreply.github.com>
2025-02-13 18:00:01 +0100

    Rôle : Appliquer des exigences strictes pour les mots de passe.

---
+++ Password polices.sh
@@ -0,0 +1,6 @@
+echo "minlen = 14" >> /etc/security/pwquality.conf
+echo "dcredit = -1" >> /etc/security/pwquality.conf
+echo "ucredit = -1" >> /etc/security/pwquality.conf
+echo "ocredit = -1" >> /etc/security/pwquality.conf
+echo "lcredit = -1" >> /etc/security/pwquality.conf
+echo "Password complexity enforced."
