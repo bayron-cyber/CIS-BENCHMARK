8e05f4655e7201a6d7759780332262c765de1109
bayron-cyber <emmanueladounvo408@gmail.com>
2025-02-13 15:45:48 +0100

    Rôle : Désactiver squashfs, un système de fichiers compressé en lecture seule.

--- squashfs.sh
+++ squashfs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 echo "install squashfs /bin/false" > /etc/modprobe.d/squashfs.conf
 echo "blacklist squashfs" >> /etc/modprobe.d/squashfs.conf
 modprobe -r squashfs
